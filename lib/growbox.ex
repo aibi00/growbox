65f10d9312d75076d6cf08558a3dd779466a0d48
Mario Uher <uher.mario@gmail.com>
2020-12-19 17:08:04 +0100

    Initialize application

---
+++ lib/growbox.ex
@@ -0,0 +1,18 @@
+defmodule Growbox do
+  @moduledoc """
+  Documentation for `Growbox`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Growbox.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
